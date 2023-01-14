2f23f414837ef3eedf7c4e0d6977c6d3444ebcac
himanushi <himanushi@users.noreply.github.com>
2023-01-14 14:27:21 +0900

    icon 対応

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,10 @@
+<script lang="ts">
+	import Icon from '~/components/icon.svelte';
+</script>
+
 <ion-header>
 	<ion-toolbar>
 		<ion-title>Header</ion-title>
 	</ion-toolbar>
 </ion-header>
-<ion-content> aaa </ion-content>
+<ion-content> <Icon name={'10k'} /> </ion-content>
