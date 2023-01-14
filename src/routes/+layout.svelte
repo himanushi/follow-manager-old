ea1a98f39d476d56a5540bffd602627efb943ede
himanushi <himanushi@users.noreply.github.com>
2023-01-14 14:08:28 +0900

    ionic 対応

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,15 @@
-<slot />
+<script lang="ts">
+	import { initialize } from '@ionic/core';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		initialize({
+			animated: true,
+			mode: 'ios'
+		});
+	});
+</script>
+
+<ion-app>
+	<slot />
+</ion-app>
